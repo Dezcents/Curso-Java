28bd4cd7c06648bb6694ba3965a89f1837773156
Marcus Gularte <marcus.gbatista@gmail.com>
2021-09-10 20:56:06 -0300

    aula07
    Operadores Aritméticos e classe Math. Operadores Ternários, conceito de pré e pós incremento. Operadores de atribuição. Exponenciação e operações avançadas. Arredondamentos. Gerador de números randômicos.

---
+++ aula07/OperadorTernario/src/operadorternario/OperadorTernario.java
@@ -0,0 +1,11 @@
+package operadorternario;
+public class OperadorTernario {
+    public static void main(String[] args) {
+        int n1,n2,res;
+        n1 = 4;
+        n2 = 8;
+        res =(n1>n2)?n1:n2;
+        System.out.println(res);
+    }
+    
+}
